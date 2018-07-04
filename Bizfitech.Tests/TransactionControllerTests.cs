1c45f3a07d85c75dea3d6776712624e6c0a083bc
Krunal <p13205072@my365.dmu.ac.uk>
2018-07-04 21:14:21 +0100

    Added Microsoft.Net.Http nuget to test project and removed unused directives

--- Bizfitech.Tests/TransactionControllerTests.cs
+++ Bizfitech.Tests/TransactionControllerTests.cs
@@ -2,12 +2,7 @@
 using Bizfitech.Web.Interfaces;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bizfitech.Tests
 {
